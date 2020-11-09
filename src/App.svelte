fbd7549a5b1d103cd67b59cc430604c112a4ad42
Chris Chapman <42548502+ccchapman@users.noreply.github.com>
2020-11-08 22:03:19 -0800

    feat: Setup Svelte scaffolding

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Fira+Mono&family=Playfair+Display:wght@400;500;700&display=swap');
+
+  :global(body) {
+    background-color: hsl(177, 59%, 13%);
+    color: hsl(180, 25%, 98%);
+    font-family: 'Fira Mono';
+    font-size: 1.125rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    overflow: hidden;
+    margin: 0;
+    padding: 0;
+  }
+</style>
+
+<script>
+  import Headline from "./components/Headline.svelte";
+  import Shape from "./components/Shape.svelte";
+</script>
+
+<div>
+  <Shape />
+
+  <Headline>
+    Chris Chapman
+  </Headline>
+
+  <p>
+    Web Developer in Vancouver, BC
+  </p>
+</div>
---
+++ src/components/Headline.svelte
@@ -0,0 +1,12 @@
+<style>
+  h1 {
+    font-family: 'Playfair Display', serif;
+    font-size: 1.875em;
+    font-weight: 500;
+    letter-spacing: 0.0625rem;
+  }
+</style>
+
+<h1>
+  <slot></slot>
+</h1>
